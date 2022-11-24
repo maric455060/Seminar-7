f841f1c8a6afb2989a5758d4a1264141aceb2da7
maric455060 <111137280+maric455060@users.noreply.github.com>
2022-11-24 11:55:48 +0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,11 @@
+def get_info ():
+    info = []
+    last_name = input('Введите фамилию: ')
+    info.append(last_name)
+    first_name = input('Введите имя: ')
+    info.append(first_name)
+    phone_number = input('Введите номер телефона: ')     
+    info.append(phone_number)
+    description = input('Введите описание: ')
+    info.append(description)
+    return infos
